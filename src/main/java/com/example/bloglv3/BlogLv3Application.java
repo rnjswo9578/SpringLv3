a1ac1c7554c7081393f969ccb3060a56fd13a9f8
rnjswo9578 <rnjswo153486@gmail.com>
2023-04-21 20:10:00 +0900

    first commit

---
+++ src/main/java/com/example/bloglv3/BlogLv3Application.java
@@ -0,0 +1,13 @@
+package com.example.bloglv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogLv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bloglv3/BlogLv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloglv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
